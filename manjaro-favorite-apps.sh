d6e2de58941b9646ad0916e7551789f89c6299d9
Miguel Rios <miguelriosoliveira@gmail.com>
2017-08-29 16:54:57 -0300

    Update and rename manjaro-favorite-apps.md to manjaro-favorite-apps.sh

---
+++ manjaro-favorite-apps.sh
@@ -0,0 +1,13 @@
+# update mirrors
+sudo pacman-mirrors -g
+# synchonize repositories and update installed packages
+sudo pacman -Syyu
+# only for slow HDs -- not recommended for SSDs!
+if [[ $@ == "--optimize" ]]; then
+	sudo pacman-optimize && sync
+fi
+# apps from standard repositories
+sudo pacman -S git tk bash-completion nodejs python-pip postgresql dbeaver asciiquarium sl 
+# apps from AUR
+yaourt -Syua
+yaourt -S --noconfirm tilix-bin webstorm pycharm-professional datagrip sublime-text-dev visual-studio-code atom-editor-bin google-chrome vivaldi spotify franz-bin
